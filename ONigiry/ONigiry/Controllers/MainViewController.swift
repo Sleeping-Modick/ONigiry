1b0c69726a6953747f912b52400eb11016f062e4
MOMO <jakkujakku98@gmail.com>
2023-09-12 01:15:55 +0900

    [FEAT]: Pod 파일 추가 & 워크 스페이스 추가

--- ONigiry/ONigiry/Controllers/MainViewController.swift
+++ ONigiry/ONigiry/Controllers/MainViewController.swift
@@ -5,11 +5,30 @@
 //  Created by (^ㅗ^)7 iMac on 2023/09/11.
 //
 
+import SnapKit
 import UIKit
 
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupUI()
+    }
+
+    let textButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TEST BUTTON", for: .normal)
+        button.backgroundColor = UIColor.systemRed
+        button.layer.cornerRadius = 10
+        return button
+    }()
+
+    func setupUI() {
+        view.addSubview(textButton)
+        textButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(view)
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
     }
 }
--- ONigiry/ONigiry/Controllers/ProfileViewController.swift
+++ ONigiry/ONigiry/Controllers/ProfileViewController.swift
@@ -6,10 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupUI()
+    }
+    
+    let textButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TEST BUTTON", for: .normal)
+        button.backgroundColor = UIColor.systemRed
+        button.layer.cornerRadius = 10
+        return button
+    }()
+
+    func setupUI() {
+        view.addSubview(textButton)
+        textButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(view)
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
     }
 }
